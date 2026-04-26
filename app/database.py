36005145aeec9f014ee3bb03094008c740e9fc53
gorow <kamblegaurav07@gmail.com>
2026-04-26 12:18:46 +0530

    final frontend fix

--- app/database.py
+++ app/database.py
@@ -1,23 +1,12 @@
 from pymongo import MongoClient
 import os
-from dotenv import load_dotenv
 
-# Load .env
-load_dotenv()
-
-# Get Mongo URI
 MONGO_URI = os.getenv("MONGO_URI")
 
-# Debug print (remove later)
-print("Mongo URI:", MONGO_URI)
-
-# Connect to MongoDB
 client = MongoClient(MONGO_URI)
 
-# Database
 db = client["pharmacy"]
 
-# Collections
 stores = db["stores"]
 medicines = db["medicines"]
 orders = db["orders"]
