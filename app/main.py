ed5adf96f8093cbd2be1056ad8207d65bb667e26
gorow <kamblegaurav07@gmail.com>
2026-04-28 23:07:44 +0530

    fixed CORS issue

--- app/main.py
+++ app/main.py
@@ -1,20 +1,26 @@
-from fastapi import FastAPI, Request
-from app.routes import auth, medicine, order
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.routes import auth, medicine, order
 
 app = FastAPI()
 
-# Middleware for multi-tenant
+# ✅ CORS FIX (VERY IMPORTANT)
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:5500",
+    "https://wowmedical.netlify.app"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all (for now)
+    allow_origins=["*"],  # 👈 for testing (allow all)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Routes
+# Routers
 app.include_router(auth.router, prefix="/auth")
 app.include_router(medicine.router, prefix="/medicines")
 app.include_router(order.router, prefix="/orders")
